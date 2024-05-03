cf73c3ffeec782b2463b58f9ffd23098888af8a7
phoberti <pedropes2010pedropes@gmail.com>
2024-05-03 02:08:42 -0300

    Primeiro Commit: Teste Passou

---
+++ src/test/java/DollarTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DollarTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
