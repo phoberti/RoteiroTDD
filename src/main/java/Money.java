dcaaa6359783e5db030aa6bfacea5bcb42cb54ab
phoberti <pedropes2010pedropes@gmail.com>
2024-05-04 00:39:51 -0300

    Quinto Commit : Todos os testes passaram e subclasses removidas

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,20 +1,13 @@
-abstract class Money {
+class Money {
     protected int amount;
     protected String currency;
 
-    abstract Money times(int multiplier);
-
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
-    }
-
     static Money dollar(int amount)  {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     String currency() {
@@ -25,4 +18,14 @@ abstract class Money {
         this.amount = amount;
         this.currency = currency;
     }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
 }
